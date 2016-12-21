aa67acb62837a06b38977a32553d7ecbdbfa343b
bcrusu <bcrusu@users.noreply.github.com>
2016-12-21 19:22:47 +0200

    added cassandra cluster management script

---
+++ scripts/cassandra_cluster.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+SCRIPTDIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+
+CASSANDRA_VERSION="3.9"
+CASSANDRA_CLUSTER_NAME="pregel"
+CASSANDRA_NODE_COUNT=2
+
+create_cluster() {
+	ccm create $CASSANDRA_CLUSTER_NAME -v $CASSANDRA_VERSION
+	ccm populate -I 127.0.100.%d -n $CASSANDRA_NODE_COUNT
+}
+
+start_cluster() {
+	ccm switch $CASSANDRA_CLUSTER_NAME
+    ccm start --jvm_arg="-Xms256M -Xmx256M"
+}
+
+stop_cluster() {
+	ccm stop $CASSANDRA_CLUSTER_NAME
+}
+
+clean() {
+	ccm remove $CASSANDRA_CLUSTER_NAME
+}
+
+case "$1" in
+	create)
+		echo "Creating cluster..."
+		create_cluster
+		echo "Done."
+		;;
+	start)
+		echo "Starting..."
+		start_cluster
+		echo "Done."
+		;;
+	stop)
+		echo "Stopping..."
+		stop_cluster
+		echo "Done."
+		;;
+	clean)
+		echo "Cleaning cluster dir..."
+		clean
+		echo "Done."
+		;;
+	*)
+		echo "Usage: cluster {start|stop|create|clean}"
+		exit 1
+		;;
+esac

f840203aa7e8c59b54819d5690165230f42cf6ad
bcrusu <bcrusu@users.noreply.github.com>
2016-12-28 21:38:45 +0200

    cleanup

--- dataLoader/parsers/parsers.go
+++ dataLoader/parsers/parsers.go
@@ -1,7 +1,35 @@
 package parsers
 
-import "github.com/bcrusu/pregel"
+import (
+	"flag"
+	"fmt"
+	"io"
+
+	"github.com/bcrusu/pregel"
+)
 
 type Parser interface {
 	Next() *pregel.Edge
+}
+
+var (
+	parserType string
+)
+
+func init() {
+	flag.StringVar(&parserType, "parser", "dimacs", "Parser type. Available parsers: dimacs")
+}
+
+func NewParser(reader io.Reader) (Parser, error) {
+	var parser Parser
+	var err error
+
+	switch parserType {
+	case "dimacs":
+		parser = NewDimacsParser(reader)
+	default:
+		err = fmt.Errorf("Invalid parser type '%s'", parserType)
+	}
+
+	return parser, err
 }
